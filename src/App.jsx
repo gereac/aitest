5a99772bade91120402a93a61e6527c9acacd790
Catalin Gerea <catalin.gerea@urbanandmainlines.com>
2026-04-06 19:33:33 +0300

    created dummy hello world react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "4rem",
+        fontFamily: "sans-serif",
+      }}
+    >
+      <h1>Hello World Foo</h1>
+    </div>
+  );
+}
+
+export default App;
