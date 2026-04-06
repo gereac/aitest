38bf2b1d60a6a06a6770148f7b5043292425873d
Catalin Gerea <catalin.gerea@urbanandmainlines.com>
2026-04-06 21:09:31 +0300

    corrected deployment and workflow settings

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",
+  server: {
+    port: 5174,
+    strictPort: true,
+    host: true,
+  },
 });
